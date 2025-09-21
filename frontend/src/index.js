0622c57dcce5dde34ad8250bc0f10cef78aaab3f
kaynranger-dot <kaynranger@gmail.com>
2025-09-21 11:20:24 +0800

    Updated index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,17 @@
 import React from "react";
-import { createRoot } from 'react-dom/client';
-import "./index.css";  
-import App from "./App"; 
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
+import { AuthProvider } from "./contexts/AuthContext"; // 👈 import here
 
-const root = createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </Router>
   </React.StrictMode>
 );
